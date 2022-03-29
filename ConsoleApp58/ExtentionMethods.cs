d7f3b20e46d20b7457b91532ee1983bdfd5cb8cb
unknown <tu7gbzmx6@code.edu.az>
2022-03-29 10:18:44 +0400

    ConsoleApp58

---
+++ ConsoleApp58/ExtentionMethods.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp58
+{
+    static class ExtentionMethods
+    {
+        public static bool IsOdd(this int num)
+        {
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsEven(this int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsContainsDigit(this string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static string ToCapitalize(this string word)
+        {
+            return word.Substring(0, 1).ToUpper() + word.Substring(0).ToLower();
+
+        }
+
+        public static int GetValueIndexes(this string str, char c)
+        {
+            int count = 0;
+            foreach (var item in str)
+            {
+                if (item == c)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ ConsoleApp58/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp58
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 9;
+            Console.WriteLine(a.IsOdd());
+            Console.WriteLine("--------------------------------------");
+            int b = 8;
+            Console.WriteLine(b.IsEven());
+            Console.WriteLine("--------------------------------------");
+            string c = "Salam12";
+            Console.WriteLine(c.IsContainsDigit());
+            Console.WriteLine("--------------------------------------");
+            string d = "AZERBAYCAN TEXNIKI UNIVERSITETI";
+            Console.WriteLine(d.ToCapitalize());
+            Console.WriteLine("--------------------------------------");
+            string e = "Azerbaycan";
+            Console.WriteLine(e.GetValueIndexes('D'));
+            Console.WriteLine(e.GetValueIndexes('A'));
+        }
+    }
+}
